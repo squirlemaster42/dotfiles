badb90a2d3dea2ce220bad39b4d677d68e094ed0
squirlemaster42 <jakobmisbach8@gmail.com>
2026-07-23 22:21:36 -0400

    move file to correct spot

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -16,20 +16,34 @@ function M.get_capabilities()
     return capabilities
 end
 
--- Global on_attach function with all your existing key mappings
+-- Set diagnostic keymaps globally (works without LSP)
+local diagnostic_opts = {remap = false}
+vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, diagnostic_opts)
+vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, diagnostic_opts)
+vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, diagnostic_opts)
+
+-- LSP-specific on_attach function
+local function set_diagnostic_keymaps(bufnr)
+    local opts = {buffer = bufnr, remap = false}
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+end
+
+-- LSP-specific on_attach function
 function M.on_attach(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
     
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.symbols() end, opts)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    
+    -- Also ensure diagnostic keymaps are set buffer-locally
+    set_diagnostic_keymaps(bufnr)
 end
 
 -- Setup function to enable all servers
@@ -61,14 +75,6 @@ function M.setup()
         } 
     })
     
-    vim.lsp.config('*', {
-        capabilities = M.get_capabilities(),
-        on_attach = M.on_attach,
-        flags = {
-            debounce_text_changes = 150,
-        }
-    })
-    
     local servers = {
         "ts_ls",
         "rust_analyzer", 
@@ -80,6 +86,9 @@ function M.setup()
     
     for _, server in ipairs(servers) do
         local config = require('lsp.' .. server)
+        config.capabilities = M.get_capabilities()
+        config.on_attach = M.on_attach
+        config.flags = { debounce_text_changes = 150 }
         vim.lsp.config(server, config)
     end
     
